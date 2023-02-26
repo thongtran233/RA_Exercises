d57e6d2dd69af250481e0127ec02d58aad19b9b2
thongtran233 <thongtran230395@gmail.com>
2023-02-26 21:42:27 +0900

    BaiTap

---
+++ 1-Model 1_HTML, CSS, JS/Session 6 - JavaScript/BaiTap1/Bai1.js
@@ -0,0 +1,22 @@
+// Bai1
+// let VatLy = parseInt(prompt("Nhập điểm Vật Lý "));
+// let HoaHoc = parseInt(prompt("Nhập điểm Hóa Học "));
+// let SinhHoc = parseInt(prompt("Nhập điểm Sinh Học "));
+// let Tong = VatLy + HoaHoc + SinhHoc;
+// let TrungBinh = Tong / 3;
+// document.write(" Điểm Tổng : ", Tong);
+// document.write(" Điểm trung bình : ", TrungBinh);
+
+// Bai 2
+// let doC = parseInt(prompt("Nhập nhiệt độ C "));
+// let doF = (doC * 9) / 5 + 32;
+// document.write(" Nhiệt độ bằng độ C:  ", doC);
+// document.write(" Nhiệt độ bằng độ F:  ", doF);
+
+// Bai 3
+// let banKinh = parseInt(prompt("Nhập bán kính hình tròn "));
+// const soPi = 3.14;
+// let chuVi = 2 * soPi * banKinh;
+// let dienTich = soPi * banKinh * banKinh;
+// document.write(" Chu vi hình tròn:  ", chuVi);
+// document.write(" Diện tích hình tròn:  ", dienTich);
