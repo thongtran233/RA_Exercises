23bba1a1481df7b229e6ddbf23e7dbb0f506df8b
thongtran233 <thongtran230395@gmail.com>
2023-03-01 11:19:08 +0900

    BaiTap

--- 1-Model 1_HTML, CSS, JS/Session 7 - Condition, loop/BaiTap2/Bai2.js
+++ 1-Model 1_HTML, CSS, JS/Session 7 - Condition, loop/BaiTap2/Bai2.js
@@ -9,47 +9,36 @@ function dayOfMonth() {
       result.innerHTML = "Tháng 1 có 31 ngày";
       break;
     case 2:
-      console.log("Tháng " + number);
       result.innerHTML = "Tháng 2 có 28 hoặc 29 ngày";
       break;
     case 3:
-      console.log("Tháng " + number);
       result.innerHTML = "Tháng 3 có 31 ngày";
       break;
     case 5:
-      console.log("Tháng " + number);
       result.innerHTML = "Tháng 5 có 31 ngày";
       break;
     case 7:
-      console.log("Tháng " + number);
       result.innerHTML = "Tháng 7 có 31 ngày";
       break;
     case 8:
-      console.log("Tháng " + number);
       result.innerHTML = "Tháng 8 có 31 ngày";
       break;
     case 10:
-      console.log("Tháng " + number);
       result.innerHTML = "Tháng 10 có 31 ngày";
       break;
     case 12:
-      console.log("Tháng " + number);
       result.innerHTML = "Tháng 12 có 31 ngày";
       break;
     case 4:
-      console.log("Tháng " + number);
       result.innerHTML = "Tháng 4 có 30 ngày";
       break;
     case 6:
-      console.log("Tháng " + number);
       result.innerHTML = "Tháng 6 có 30 ngày";
       break;
     case 9:
-      console.log("Tháng " + number);
       result.innerHTML = "Tháng 9 có 30 ngày";
       break;
     case 11:
-      console.log("Tháng " + number);
       result.innerHTML = "Tháng 11 có 30 ngày";
       break;
     default:
